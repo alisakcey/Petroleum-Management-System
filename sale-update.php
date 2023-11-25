62120f5eae413163f1b0d5d510b0785f18cfef2f
alisakcey <alishakunwar611@gmail.com>
2023-11-25 21:20:28 +0545

    initial commit

---
+++ sale-update.php
@@ -0,0 +1,114 @@
+<div class="table-responsive table--no-card m-b-60">
+                                    <table class="table table-borderless table-striped table-earning">
+                                        <thead>
+                                            <tr>
+                                                <th>Date</th>
+                                                <th>Order ID</th>
+                                                <th>Vechile No.</th>
+                                                <th class="text-right">Type</th>
+                                                <th class="text-right">Rate</th>
+                                                <th class="text-right">Quantity (Ltr)</th>
+                                                <th class="text-right">Total</th>
+                                                <th class="text-right">Status</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>2018-09-29 05:57</td>
+                                                <td>100398</td>
+                                                <td>Lu 2 Kha 8081</td>
+                                                <td class="text-right">Desiel</td>
+                                                <td class="text-right">Rs. 180</td>
+                                                <td class="text-right">1</td>
+                                                <td class="text-right">Rs.180.00</td>
+                                                <td class="text-right">
+                                                    <span class="badge rounded-pill bg-success text-light">Paid</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-28 01:22</td>
+                                                <td>100397</td>
+                                                <td>Ja 5 Pa 76</td>
+                                                <td class="text-right">Petrol</td>
+                                                <td class="text-right">Rs. 195.7</td>
+                                                <td class="text-right">3.5</td>
+                                                <td class="text-right">Rs.504.14</td>
+                                                <td class="text-right">
+                                                    <span class="badge rounded-pill bg-warning text-light">Due</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-27 02:12</td>
+                                                <td>100396</td>
+                                                <td>Lu 2 Kha 8081</td>
+                                                <td class="text-right">Desiel</td>
+                                                <td class="text-right">Rs. 180</td>
+                                                <td class="text-right">1</td>
+                                                <td class="text-right">Rs.180.00</td>
+                                                <td class="text-right">
+                                                    <span class="badge rounded-pill bg-danger text-light">Paid</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-26 23:06</td>
+                                                <td>100395</td>
+                                                <td>Lu 2 Kha 8081</td>
+                                                <td class="text-right">Desiel</td>
+                                                <td class="text-right">Rs. 180</td>
+                                                <td class="text-right">1</td>
+                                                <td class="text-right">Rs.180.00</td>
+                                                <td class="text-right">
+                                                    <span class="badge rounded-pill bg-success text-light">Paid</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-25 19:03</td>
+                                                <td>100393</td>
+                                                <td>Lu 2 Kha 8081</td>
+                                                <td class="text-right">Desiel</td>
+                                                <td class="text-right">Rs. 180</td>
+                                                <td class="text-right">1</td>
+                                                <td class="text-right">Rs.180.00</td>
+                                                <td class="text-right">
+                                                    <span class="badge rounded-pill bg-success text-light">Paid</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-29 05:57</td>
+                                                <td>100392</td>
+                                                <td>Lu 2 Kha 8081</td>
+                                                <td class="text-right">Desiel</td>
+                                                <td class="text-right">Rs. 180</td>
+                                                <td class="text-right">1</td>
+                                                <td class="text-right">Rs.180.00</td>
+                                                <td class="text-right">
+                                                    <span class="badge rounded-pill bg-success text-light">Paid</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-24 19:10</td>
+                                                <td>100391</td>
+                                                <td>Lu 2 Kha 8081</td>
+                                                <td class="text-right">Desiel</td>
+                                                <td class="text-right">Rs. 180</td>
+                                                <td class="text-right">1</td>
+                                                <td class="text-right">Rs.180.00</td>
+                                                <td class="text-right">
+                                                    <span class="badge rounded-pill bg-success text-light">Paid</span>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>2018-09-22 00:43</td>
+                                                <td>100393</td>
+                                                <td>Lu 2 Kha 8081</td>
+                                                <td class="text-right">Desiel</td>
+                                                <td class="text-right">Rs. 180</td>
+                                                <td class="text-right">1</td>
+                                                <td class="text-right">Rs.180.00</td>
+                                                <td class="text-right">
+                                                    <span class="badge rounded-pill bg-success text-light">Paid</span>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
